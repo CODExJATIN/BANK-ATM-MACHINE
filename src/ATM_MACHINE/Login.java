97e4dc12c5acfb010cb0118a479ae52b87b3c4a1
JATIN <117251224+CODExJATIN@users.noreply.github.com>
2023-09-29 00:58:41 +0530

    Initial commit

---
+++ src/ATM_MACHINE/Login.java
@@ -0,0 +1,113 @@
+package ATM_MACHINE;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.sql.*;
+
+public class Login extends JFrame implements ActionListener{
+    JLabel l1,l2,l3;
+    JTextField tf1;
+    JPasswordField pf2;
+    JButton signin,clear,b3;
+
+    Login(){
+        setLayout(null);
+        getContentPane().setBackground(Color.LIGHT_GRAY);
+        setSize(800,480);
+        setLocation(200,200);
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/logo.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel l11 = new JLabel(i3);
+        l11.setBounds(70, 10, 100, 100);
+        add(l11);
+
+        l1 = new JLabel("WELCOME TO ATM");
+        l1.setFont(new Font("Osward", Font.BOLD, 38));
+        l1.setBounds(200,40,450,40);
+        add(l1);
+
+        l2 = new JLabel("Card No:");
+        l2.setFont(new Font("Raleway", Font.BOLD, 28));
+        l2.setBounds(125,150,375,30);
+        add(l2);
+
+        tf1 = new JTextField(15);
+        tf1.setBounds(300,150,230,30);
+        tf1.setFont(new Font("Arial", Font.BOLD, 14));
+        add(tf1);
+
+        l3 = new JLabel("PIN:");
+        l3.setFont(new Font("Raleway", Font.BOLD, 28));
+        l3.setBounds(125,220,375,30);
+        add(l3);
+
+        pf2 = new JPasswordField(15);
+        pf2.setFont(new Font("Arial", Font.BOLD, 14));
+        pf2.setBounds(300,220,230,30);
+        add(pf2);
+
+        signin = new JButton("SIGN IN");
+        signin.setBackground(Color.BLACK);
+        signin.setForeground(Color.WHITE);
+        signin.setFont(new Font("Arial", Font.BOLD, 14));
+        signin.setBounds(300,300,100,30);
+        signin.addActionListener(this);
+        add(signin);
+
+        clear = new JButton("CLEAR");
+        clear.setBackground(Color.BLACK);
+        clear.setForeground(Color.WHITE);
+        clear.setFont(new Font("Arial", Font.BOLD, 14));
+        clear.setBounds(430,300,100,30);
+        clear.addActionListener(this);
+        add(clear);
+
+        b3 = new JButton("SIGN UP");
+        b3.setBackground(Color.BLACK);
+        b3.setForeground(Color.WHITE);
+        b3.setFont(new Font("Arial", Font.BOLD, 14));
+        b3.setBounds(300,350,230,30);
+        b3.addActionListener(this);
+        add(b3);
+
+
+
+
+        setVisible(true);
+
+    }
+    public void actionPerformed(ActionEvent ae){
+        /*try{
+            if(ae.getSource()==signin){
+                Conn c1 = new Conn();
+                String cardno  = tf1.getText();
+                String pin  = pf2.getText();
+                String q  = "select * from login where cardno = '"+cardno+"' and pin = '"+pin+"'";
+
+                ResultSet rs = c1.s.executeQuery(q);
+                if(rs.next()){
+                    setVisible(false);
+                    new Transactions(pin).setVisible(true);
+                }else{
+                    JOptionPane.showMessageDialog(null, "Incorrect Card Number or PIN");
+                }
+            }else if(ae.getSource()==clear){
+                tf1.setText("");
+                pf2.setText("");
+            }else if(ae.getSource()==b3){
+                setVisible(false);
+                new Signup().setVisible(true);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }*/
+    }
+    public static void main(String[] args){
+        new Login();
+    }
+
+
+}
